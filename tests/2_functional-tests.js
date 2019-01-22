226d3b57a87b426a9353dd6abcefcd1afd471292
Glitch (five-chai) <none>
2019-01-22 20:52:36 +0000

    🎟🍢 Updated with Glitch

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -170,7 +170,7 @@ suite('Functional Tests', function() {
 
   // On Gomix we'll use this setting
   /** ### Copy your project's url here  ### **/
-  Browser.site = 'https://sincere-cone.gomix.me'; 
+  Browser.site = 'https://five-chai.glitch.me/'; 
   
   // If you are testing on a local environment replace the line above  with 
   // Browser.localhost('example.com', (process.env.PORT || 3000));
@@ -250,14 +250,18 @@ suite('Functional Tests', function() {
             // pressButton is Async.  Waits for the ajax call to complete...
 
             // assert that status is OK 200
+          browser.assert.success();
 
             // assert that the text inside the element 'span#name' is 'Marco'
-
+          browser.assert.text('span#name', 'Cristoforo');
+    
             // assert that the text inside the element 'span#surname' is 'Polo'
+          browser.assert.text('span#surname', 'Colombo');
 
             // assert that the element(s) 'span#dates' exist and their count is 1
+          browser.assert.element('span#dates', 1);
             
-            assert.fail();
+            //assert.fail();
             
             done();   // It's an async test, so we have to call 'done()''
           });
